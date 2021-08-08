348584cc1ceb90f24825fa68a062e31401084629
Saurav <sp.gharti@gmail.com>
2021-08-08 10:49:02 +0545

    activate loadmore button

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ import Grid from './Grid'
 import Thumb from './Thumb'
 import Spinner from './Spinner'
 import SearchBar from './SearchBar'
+import Button from './Button'
 
 //Hook
 import { useHomeFetch } from '../hooks/useHomeFetch'
@@ -19,9 +20,11 @@ import NoImage from '../images/no_image.jpg'
 
 
 const Home = () =>{
-    const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch()
+    const { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore } = useHomeFetch()
 
     console.log(state)
+
+    if (error) return <div>Something went wrong ...</div>
 
     return (
         <>
@@ -48,7 +51,10 @@ const Home = () =>{
                 />
             ))}
         </Grid>
-        <Spinner />
+        {loading && <Spinner/>}
+        {state.page < state.total_pages && !loading && (
+            <Button text='Load More' callback={() => setIsLoadingMore(true)}></Button>
+        )}
         </>
     )
 }
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -13,6 +13,7 @@ export const useHomeFetch = () => {
     const [state, setState] = useState(initialState)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
+    const [isLoadingMore, setIsLoadingMore] = useState(false)
 
     console.log(searchTerm)
 
@@ -34,10 +35,18 @@ export const useHomeFetch = () => {
         setLoading(false)
     }
 
+    // Search
     useEffect(() => {
         setState(initialState)
         fetchMovies(1, searchTerm)
     }, [searchTerm])
 
-    return { state, loading, error, searchTerm, setSearchTerm}
+    // Load More
+    useEffect(() => {
+        if (!isLoadingMore) return
+        fetchMovies(state.page + 1, searchTerm)
+        setIsLoadingMore(false)
+    }, [isLoadingMore, searchTerm, state.page])
+
+    return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore}
 }
