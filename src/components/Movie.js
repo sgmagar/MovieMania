6f96d80480cd7561babb7beabf5e872817cc9cae
Saurav <sp.gharti@gmail.com>
2022-12-23 14:59:21 +0545

    finish movie detail page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,6 +4,9 @@ import {useParams} from 'react-router-dom'
 import {IMAGE_BASE_URL, POSTER_SIZE} from '../config'
 // Components
 import BreadCrumb from './BreadCrumb'
+import MovieInfo from './MovieInfo'
+import MovieInfoBar from './MovieInfoBar'
+import Actor from "./Actor"
 import Grid from './Grid'
 import Spinner from './Spinner'
 //Hooks
@@ -20,6 +23,20 @@ const Movie = () => {
     return (
         <>
             <BreadCrumb movieTitle={movie.original_title}/>
+            <MovieInfo movie={movie}/>
+            <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue}/>
+            <Grid header="Actors">
+                {movie.actors && movie.actors.map(actor => (
+                    <Actor 
+                        key={actor.credit_id} 
+                        name={actor.name} 
+                        character={actor.character} 
+                        imageUrl={
+                            actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`: NoImage
+                        } 
+                    />
+                ))}
+            </Grid>
         </>
     )
 }
